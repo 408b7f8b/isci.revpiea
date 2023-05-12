f2fe85940db7f718393b4b77dc03b5f4334662c3
408b7f8b <davbr@tutanota.com>
2023-05-12 18:15:50 +0200

    init commit

---
+++ Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Linq;
+using isci;
+using isci.Allgemein;
+using isci.Daten;
+using isci.Beschreibung;
+using isci.Anwendungen;
+using System.Collections.Generic;
+
+namespace revpiea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var konfiguration = new Parameter("konfiguration.json");
+            
+            var structure = new Datenstruktur(konfiguration.OrdnerDatenstruktur);
+
+            RevPiZugriff.SystemkonfigurationLesen();
+            RevPiZugriff.EinUndAusgängeAufstellen();
+            RevPiZugriff.control.Open();
+
+            var dm = new Datenmodell(konfiguration.Identifikation);
+
+            var Ausgaenge = new Dictionary<dtInt32, ioObjekt>();
+            var Eingaenge = new Dictionary<ioObjekt, dtInt32>();
+
+            foreach (var eintrag_ in RevPiZugriff.Eingänge)
+            {
+                var eintrag = eintrag_.Value.EintragErstellen("");
+                dm.Dateneinträge.Add(eintrag);
+                Eingaenge.Add(eintrag_.Value, eintrag);
+            }
+
+            foreach (var eintrag_ in RevPiZugriff.Ausgänge)
+            {
+                var eintrag = eintrag_.Value.EintragErstellen("");
+                dm.Dateneinträge.Add(eintrag);
+                Ausgaenge.Add(eintrag, eintrag_.Value);
+            }
+
+            System.IO.File.WriteAllText(konfiguration.OrdnerDatenmodelle + "/" + konfiguration.Identifikation + ".json", Newtonsoft.Json.JsonConvert.SerializeObject(dm));
+
+            structure.DatenmodellEinhängen(dm);
+            structure.Start();
+
+            var beschreibung = new Modul(konfiguration.Identifikation, "isci.revpiea", dm.Dateneinträge);
+            beschreibung.Name = "RevPiEA Ressource " + konfiguration.Identifikation;
+            beschreibung.Beschreibung = "Modul zur EA-Integration von RevPi";
+            beschreibung.Speichern(konfiguration.OrdnerBeschreibungen + "/" + konfiguration.Identifikation + ".json");
+
+            var Zustand = new dtZustand(konfiguration.OrdnerDatenstruktur);
+            Zustand.Start();
+
+            while(true)
+            {
+                Zustand.Lesen();
+                var erfüllteTransitionen = konfiguration.Ausführungstransitionen.Where(a => a.Eingangszustand == (System.Int32)Zustand.value);
+                if (erfüllteTransitionen.Count<Ausführungstransition>() > 0)
+                {
+                    if (erfüllteTransitionen.ElementAt(0).Eingangszustand == (System.Int32)Zustand.value)
+                    {
+                        foreach (var Eingang in Eingaenge)
+                        {
+                            object o = null;
+                            if (Eingang.Key.Zustandlesen(out o))
+                            {
+                                switch (Eingang.Key.typ)
+                                {
+                                    case ioObjekt.Typ.BOOL: Eingang.Value.value = ((bool)o ? 1 : 0); break;
+                                    case ioObjekt.Typ.BYTE: Eingang.Value.value = (int)((byte)o); break;
+                                    case ioObjekt.Typ.WORD: Eingang.Value.value = o; break;
+                                    case ioObjekt.Typ.INT: Eingang.Value.value = o; break;
+                                    default: continue;
+                                }
+                            
+                                Eingang.Value.Schreiben();
+                            }
+                        }
+                        structure.Schreiben();
+                        Zustand.value = erfüllteTransitionen.ElementAt(0).Ausgangszustand;
+                    } else if (erfüllteTransitionen.ElementAt(1).Eingangszustand == (System.Int32)Zustand.value)
+                    {
+                        foreach (var Ausgang in Ausgaenge)
+                        {
+                            Ausgang.Key.Lesen();
+                            if (Ausgang.Key.aenderung)
+                            {
+                                Console.WriteLine("Aenderung " + Ausgang.Key.Identifikation);
+                                switch (Ausgang.Value.typ)
+                                {
+                                    case ioObjekt.Typ.BOOL: Ausgang.Value.Zustandschreiben((System.Int32)Ausgang.Key.value == 1 ? true : false); break;
+                                    case ioObjekt.Typ.BYTE: Ausgang.Value.Zustandschreiben((byte)Ausgang.Key.value); break;
+                                    case ioObjekt.Typ.WORD: Ausgang.Value.Zustandschreiben((short)Ausgang.Key.value); break;
+                                    case ioObjekt.Typ.INT: Ausgang.Value.Zustandschreiben((System.Int32)Ausgang.Key.value); break;
+                                    default: continue;
+                                }
+
+                                Ausgang.Key.aenderung = false;
+                            }
+                        }
+                        Zustand.value = erfüllteTransitionen.ElementAt(1).Ausgangszustand;
+                    }
+                    
+                    Zustand.Schreiben();
+                }
+            }
+        }
+    }
+}
---
+++ config.cs
@@ -0,0 +1,172 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using IctBaden.RevolutionPi;
+using IctBaden.RevolutionPi.Configuration;
+using IctBaden.RevolutionPi.Model;
+using isci.Daten;
+
+namespace revpiea
+{
+    public static class RevPiZugriff
+    {
+        public static PiControl control = new PiControl();
+        static PiConfiguration config = new PiConfiguration();
+
+        static Systemkonfiguration Konfiguration;
+
+        public static void SystemkonfigurationLesen(string pfad = "/etc/revpi/config.rsc")
+        {
+            var inh = System.IO.File.ReadAllText(pfad).Replace("\"out\":", "\"Out\":");
+            var r = Newtonsoft.Json.JsonConvert.DeserializeObject<Systemkonfiguration>(inh);
+            Konfiguration = r;
+        }
+
+        public static Dictionary<string, ioObjekt> Eingänge;
+
+        public static Dictionary<string, ioObjekt> Ausgänge;
+
+        public static void EinUndAusgängeAufstellen()
+        {
+            Eingänge = new Dictionary<string, ioObjekt>();
+            Ausgänge = new Dictionary<string, ioObjekt>();
+
+            foreach(var device in Konfiguration.Devices)
+            {
+                foreach (var _inp in device.inp)
+                {
+                    var o = new ioObjekt(_inp.Value, device.offset);
+                    if (!o.sichtbar) continue;
+                    //Eingänge.Add(_inp.Key, o);
+                    Eingänge.Add(o.Name, o);
+                }
+                foreach (var _out in device.Out)
+                {
+                    var o = new ioObjekt(_out.Value, device.offset);
+                    if (!o.sichtbar) continue;
+                    //Ausgänge.Add(_out.Key, o);
+                    Ausgänge.Add(o.Name, o);
+                }                
+            }
+        }
+    }
+    
+
+    public class Systemkonfiguration
+    {
+        public List<Device> Devices;
+    }
+
+    public class Device
+    {
+        public int offset;
+        public Dictionary<string, List<object>> inp;
+        public Dictionary<string, List<object>> Out;
+    }
+
+    public class ioObjekt
+    {
+        public string Name;
+        public int AdresseByte;
+        public int AdresseBit;
+        public bool sichtbar;
+        private object wert;
+        public enum Typ {
+                BOOL = 1, BYTE = 8, WORD = 16, INT = 32
+        };
+        public Typ typ;
+
+        private byte[] wert_;
+
+        public ioObjekt(List<object> objektliste, int offset = 0)
+        {
+            Name = (string)objektliste[0];
+            wert = objektliste[1];
+            typ = (Typ) System.Int32.Parse((string)objektliste[2]);
+            AdresseByte = offset + System.Int32.Parse((string)objektliste[3]);
+            sichtbar = (bool)objektliste[4];
+            var AdresseBit_ = System.Int32.Parse((string)objektliste[5]);
+            var AdresseBit_g = (int) (AdresseBit_ / 8.0);
+            var AdresseBit_m = AdresseBit_ % 8;
+            AdresseBit = AdresseBit_m;
+            AdresseByte += AdresseBit_g;
+            int l = (typ == Typ.INT ? 4 : (typ == Typ.WORD ? 2 : 1));
+            wert_ = new byte[l];
+        }
+
+        public dtInt32 EintragErstellen(string praefix)
+        {
+            dtInt32 dateneintrag;
+
+            switch(typ)
+            {
+                case Typ.BOOL: dateneintrag = new dtInt32(wert_[0] != 0 ? 1 : 0, Name); break;
+                case Typ.BYTE: dateneintrag = new dtInt32(wert_[0], Name); break;
+                case Typ.INT: dateneintrag = new dtInt32((wert_[3] << 24) + (wert_[2] << 16) + (wert_[1] << 8) + wert_[0], Name); break;
+                case Typ.WORD: dateneintrag = new dtInt32((short)((wert_[1] << 8) + wert_[0]), Name); break;
+                default: return null;
+            }
+
+            return dateneintrag;
+        }
+
+        public bool Zustandlesen()
+        {
+            int l = (typ == Typ.INT ? 4 : (typ == Typ.WORD ? 2 : 1));
+
+            var wert__ = new byte[l];
+            wert__ = RevPiZugriff.control.Read(AdresseByte, l);
+
+            for (int i = 0; i < l; ++i)
+            {
+                if (wert__[i] != wert_[i])
+                {
+                    goto Raus;
+                }
+            }
+
+            return false;
+
+            Raus:
+            wert_ = wert__;
+            return true;
+        }
+
+        public bool Zustandlesen(out object o)
+        {
+            bool r = Zustandlesen();
+
+            if (!r) {
+                o = null;
+                return false;
+            }
+
+            switch(typ)
+            {
+                case Typ.BOOL: bool b = ((int)wert_[0] & (1 << AdresseBit)) != 0; o = b; break;
+                case Typ.BYTE: o = wert_[0]; break;
+                case Typ.INT: int i = (wert_[3] << 24) + (wert_[2] << 16) + (wert_[1] << 8) + wert_[0]; o = i; break;
+                case Typ.WORD: int j = (wert_[1] << 8) + wert_[0]; o = j; break;
+                default: o = null; break;
+            }
+
+            if (o != null) return true;
+
+            return false;
+        }
+
+        public void Zustandschreiben(object o)
+        {
+            switch(typ)
+            {
+                case Typ.BOOL: wert_[0] = BitConverter.GetBytes((wert_[0] & ~(1 << AdresseBit)) | ( ((bool)o ? 1 : 0) << AdresseBit))[0]; break;
+                case Typ.BYTE: wert_[0] = (byte)o; break;
+                case Typ.INT: wert_ = BitConverter.GetBytes((int)o); break;
+                case Typ.WORD: wert_ = BitConverter.GetBytes((short)o); break;
+                default: o = null; break;
+            }
+
+            RevPiZugriff.control.Write(AdresseByte, wert_);
+        }
+    }
+}
